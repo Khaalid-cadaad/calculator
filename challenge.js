94a27121bf37af4a631c999d4bb8d25bdbc83fdb
unknown <khalidabdi3101@gmail.com>
2024-09-04 23:20:53 +0300

    Done calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,35 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator (num1, num2,operator){
+
+    if(operator === "+"){
+        console.log(num1 + num2);
+    }
+
+
+    else if(operator === "-") {
+        console.log(num1 - num2);
+    }
+
+    else if(operator === "-"){
+        console.log(num1 - num2);
+    }
+
+    else if(operator === "*"){
+        console.log(num1 * num2);
+    }
+
+    else if(operator === "/"){
+        if(num2 !== 0){ 
+            console.log(num1 / num2)
+        }else{
+            console.log("error: Division by zero")
+        }
+        
+        
+    } else{
+        console.log("error invalid operator")
+    }
+calculator(10,5,"operator")
+
+}
